a4307a26da9ab9d0c08bd634e25f9242fd06cb0c
ZakharD <my_milnica@mail.ru>
2016-06-09 13:55:50 +0000

    master: fisrt project

--- src/main/java/org/eclipse/che/examples/HelloWorld.java
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -3,5 +3,6 @@ package org.eclipse.che.examples;
 public class HelloWorld {
     public static void main(String[] argvs) {
         System.out.println("Hello World!");
+        System.out.println("Привет");
     }
 }
