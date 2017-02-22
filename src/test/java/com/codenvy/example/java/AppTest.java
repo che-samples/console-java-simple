c54a04a5e7491bd07ad267d102d7a9e8eac9b98d
Marian Labuda <mlabuda@redhat.com>
2017-02-22 12:26:45 +0100

    Upgraded JUnit 3 to JUnit 4, updated test, change main method arg to vararg

--- src/test/java/com/codenvy/example/java/AppTest.java
+++ src/test/java/com/codenvy/example/java/AppTest.java
@@ -1,34 +1,18 @@
 package com.codenvy.example.java;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName
-     *         name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
+@RunWith(JUnit4.class)
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    @Test
     public void testApp() {
         assertTrue(true);
     }
