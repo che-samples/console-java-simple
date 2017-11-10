c5629945a328322b2353bf804f1ca7989c695bc8
CRISTIAN <marcelomontero47@gamil.com>
2017-11-10 21:28:00 +0000

    commit 3

--- src/main/java/org/eclipse/che/examples/HelloWorld.java
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -3,6 +3,6 @@ package org.eclipse.che.examples;
 public class HelloWorld {
     public static void main(String... argvs) {
         String a = "new bb";
-        System.out.println("Hello World   " + a + "!");
+        System.out.println("Hello World  qqqqqqqqqqq " + a + "!");
     }
 }
