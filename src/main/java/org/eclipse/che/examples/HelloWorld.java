d6eeb4722b18194acda2a165cbdf584770d5ddd3
CRISTIAN <marcelomontero47@gamil.com>
2017-11-10 21:31:24 +0000

    commit 4

--- src/main/java/org/eclipse/che/examples/HelloWorld.java
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -2,7 +2,7 @@ package org.eclipse.che.examples;
 
 public class HelloWorld {
     public static void main(String... argvs) {
-        String a = "deded";
-        System.out.println("Hello World   qqqqqqqqqqq " + a + "!");
+        String a = "wwwwww";
+        System.out.println("Hello World   ssssssssssss " + a + "!");
     }
 }
