5f4323d41b1494c7a2be7f8d420d569a20c8b5dc
svor <vsvydenk@redhat.com>
2019-10-09 15:08:49 +0300

    Add license
    Signed-off-by: svor <vsvydenk@redhat.com>

--- src/main/java/org/eclipse/che/examples/HelloWorld.java
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 package org.eclipse.che.examples;
 
 public class HelloWorld {
--- src/test/java/com/codenvy/example/java/AppTest.java
+++ src/test/java/com/codenvy/example/java/AppTest.java
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 package com.codenvy.example.java;
 
 import static org.junit.Assert.assertTrue;
